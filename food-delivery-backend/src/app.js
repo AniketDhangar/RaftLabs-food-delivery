dff7f287c1016003cf2bc35a78c31cb16e253b8c
AniketDhangar <21aniketdhangar13@gmail.com>
2026-01-31 17:48:37 +0530

    Update API base URL for production

--- food-delivery-backend/src/app.js
+++ food-delivery-backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(helmet());
-app.use(cors());
+app.use(cors(""));
 app.use(morgan());
 
 app.use('/api/', router)
